df6814fcc14975e6917a41a597616b1a34447bb0
Ritik Panchal <ritikpcl@gmail.com>
2024-10-21 15:23:42 +0530

    add portfolio files

---
+++ src/data/constants.js
@@ -0,0 +1,386 @@
+export const Bio = {
+  name: "Ritik Panchal",
+  roles: [
+    "Software Developer",
+    "Backend Developer",
+    "Fullstack Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and flexible person who loves to take on new challenges. I enjoy learning and work hard to achieve great results. With a positive attitude and a desire to grow, I am ready to make a difference and do amazing things.",
+  github: "https://github.com/Ritikpcl",
+  resume:
+    "https://drive.google.com/file/d/1GedVKmYC7Qy_PywhRPxx2O4GZ_efRdVx/view?usp=drivesdk",
+  linkedin: "https://www.linkedin.com/in/ritik-panchal-9444b0240/",
+  insta: "https://www.instagram.com/ritik_pcl/",
+  facebook: "https://www.facebook.com/ritikpcl",
+};
+
+export const skills = [
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Nest Js",
+        image: "https://docs.nestjs.com/assets/logo-small-gradient.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Express Js",
+        image: "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729488087/png-transparent-mean-solution-stack-express-js-node-js-javascript-github-text-trademark-logo-thumbnail_qdacfu.png"
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+      {
+        name: "RabbitMQ",
+        image:
+          "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729488366/png-transparent-rabbitmq-advanced-message-queuing-protocol-message-broker-message-queue-computer-servers-advanced-message-queuing-protocol-angle-text-rectangle_e6ntj3.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "https://icon.icepanel.io/Technology/svg/Tailwind-CSS.svg",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Material UI",
+        image:
+          "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729488466/png-transparent-material-ui-logo-thumbnail_uxfof1.png",
+      }
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      {
+        name: "C++",
+        image:
+          "https://isocpp.org/assets/images/cpp_logo.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "TypeScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Typescript.svg/64px-Typescript.svg.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/121px-Python-logo-notext.svg.png",
+      },
+      {
+        name: "SQL",
+        image:
+          "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729488653/png-transparent-database-icon-database-free-blue-background-blue-angle-world-thumbnail_paozql.png",
+      },
+    ],
+  },
+  {
+    title: "Core Subjects",
+    skills: [
+      {
+        name: "Data Structures and Algorithm",
+        image:
+          "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729489442/png-clipart-data-structures-and-algorithms-algorithms-data-structures-programs-computer-science-others-miscellaneous-computer-science_aoqcys.png",
+      },
+      {
+        name: "OOP",
+        image:
+          "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729489616/oop-removebg-preview_tyusbr.png",
+      },
+      {
+        name: "DBMS",
+        image:
+          "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729488653/png-transparent-database-icon-database-free-blue-background-blue-angle-world-thumbnail_paozql.png",
+      },
+      {
+        name: "Operating System",
+        image:
+          "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729489779/linux_ghfcvf.png",
+      },
+      {
+        name: "Computer Networking",
+        image:
+          "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729489937/149181_i00ijd.png",
+      },
+      {
+        name: "System Design",
+        image:
+          "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729490469/design_q6x4ja.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729488700/git-icon-2048x2048-juzdf1l5_favapg.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729488788/25231_ndspdu.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729488945/postman_hifini.png",
+      },
+      {
+        name: "Vercel",
+        image:
+          "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729489091/download_kmwrug.png",
+      },
+      {
+        name: "Studio 3t",
+        image:
+          "https://studio3t.com/wp-content/themes/s3t-2020/images/logo-pos.svg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://leena.ai/images/newsRoom/mediaKit/Logo4.webp",
+    role: "SDE 1",
+    company: "Leena AI",
+    date: "July 2024 - Present",
+    desc: " Building the HelpDesk Insights project, using GenAI and NLP APIs to cluster tickets and automate solutions, effectively reducing ticket frequency.",
+    skills: [
+      "Nest Js",
+      "TypeScript",
+      "MongoDB",
+      "Docker",
+      "AWS",
+    ]
+  },
+  {
+    id: 1,
+    img: "https://leena.ai/images/newsRoom/mediaKit/Logo4.webp",
+    role: "SDE 1 - Intern",
+    company: "Leena AI",
+    date: "Jan 2024 - June 2024",
+    desc: "Contributed to the transition of the core product, the case management system, to microservices using TypeScript, Node.js, and MongoDB for improved scalability, flexibility, and maintainability and wrote test cases for the microservices using the Jest library",
+    skills: [
+      "Node Js",
+      "Express Js",
+      "TypeScript",
+      "MongoDB",
+      "Jest",
+      "Docker",
+      "AWS",
+    ]
+  },
+  {
+    id: 2,
+    img: "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729491810/SuvidhaLogo_dxszz9.png",
+    role: "Fullstack Intern",
+    company: "Suvidha Foundation",
+    date: "May 2023 - June 2023",
+    desc: "Implemented an automated letter generation and email system, simplifying the process of creating and delivering personalized letters",
+    skills: [
+      "ReactJS",
+      "NodeJs",
+      "Express Js",
+      "MongoDB",
+      "Redux",
+      "JavaScript",
+    ],
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729492355/DTU__Delhi_official_logo_ry9dwo.png",
+    school: "Delhi Technological University",
+    date: "Oct 2020 - June 2024",
+    grade: "8.59 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Software Engineering at Delhi Technological University. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - Software Engineering",
+  },
+  {
+    id: 1,
+    img: "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729492695/logo-removebg-preview_oly7bf.png",
+    school: "Shri Daulat Ram Public Sr. Sec. School, Delhi",
+    date: "Apr 2018 - Apr 2019",
+    grade: "88.4%",
+    desc: "I completed my Class 12 high school education at Shri Daulat Ram Public Sr. Sec. School, where I studied Physics, Chemistry, Mathematics, Biology and English.",
+    degree: "CBSE - XII",
+  },
+  {
+    id: 1,
+    img: "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729492695/logo-removebg-preview_oly7bf.png",
+    school: "Shri Daulat Ram Public Sr. Sec. School, Delhi",
+    date: "Apr 2017 - Apr 2018",
+    grade: "10 CGPA",
+    desc: "I completed my Class 10 education at Shri Daulat Ram Public Sr. Sec. School, where I studied a comprehensive curriculum that included subjects such as Mathematics, Science, Social Science, English, and Hindi.",
+    degree: "CBSE - X",
+  }
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Connectify",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "It is a unique social media platform that uses OpenAI API to give users the ability to express their ideas and thoughts in a more creative way through text to image AI functionality. Credentials to login: #Email: test@gmail.com, #Password: Test@123",
+    image:
+      "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729494631/connectuify_gaa6jo.png",
+    tags: [
+      "React Js",
+      "Redux",
+      "Node Js",
+      "Express Js",
+      "MongoDB",
+      "Cloundinary",
+      "Open AI"
+    ],
+    category: "web app",
+    github: "https://github.com/Ritikpcl/Connectify-2.0",
+    webapp: "https://connectify-2-0.vercel.app",
+    member: [
+      {
+        name: "Ritik Panchal",
+        img: "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729495565/17294518140016_zibnzc.jpg",
+        linkedin: "https://www.linkedin.com/in/ritik-panchal-9444b0240/",
+        github: "https://github.com/Ritikpcl",
+      },
+    ]
+  },
+  {
+    id: 1,
+    title: "Waste Tracker",
+    date: "Aug 2022 - Nov 2022",
+    description:
+      "Created a user-friendly website facilitating efficient waste tracking for volunteers in their local area. Incorporated a selection of 7 distinct map styles, including street, satellite, and dark options",
+    image:
+      "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729495444/wasteTracker_ccvvnf.png",
+    tags: [
+      "React Js",
+      "Node Js",
+      "Express Js",
+      "MongoDB",
+      "Mapbox",
+    ],
+    category: "web app",
+    github: "https://github.com/Xaier310/wasteTracker",
+    webapp: "https://wastetracker.netlify.app",
+    member: [
+      {
+        name: "Ritik Panchal",
+        img: "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729495565/17294518140016_zibnzc.jpg",
+        linkedin: "https://www.linkedin.com/in/ritik-panchal-9444b0240/",
+        github: "https://github.com/Ritikpcl",
+      },
+      {
+        name: "Pardeep Vishkarma",
+        img: "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729495733/pardeep_xzucer.png",
+        linkedin: "https://www.linkedin.com/in/pardeep-vishkarma-66860a217/",
+        github: "https://github.com/Xaier310",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Chatify",
+    date: "April 2023 - July 2023",
+    description:
+      "Built a real-time group chat app including features for text messaging, image sharing and emojis. Implemented AI chatbot to improve communication through sentence rephrasing",
+    image:
+      "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729496929/chatify_dgci0d.png",
+    tags: [
+      "React Js",
+      "Firebase",
+      "Node Js",
+      "Express Js",
+      "Tailwind CSS",
+    ],
+    category: "web app",
+    github: "https://github.com/Ritikpcl/Chatify",
+    webapp: "https://ritik-chatify.vercel.app/",
+    member: [
+      {
+        name: "Ritik Panchal",
+        img: "https://res.cloudinary.com/dhfofv8gz/image/upload/v1729495565/17294518140016_zibnzc.jpg",
+        linkedin: "https://www.linkedin.com/in/ritik-panchal-9444b0240/",
+        github: "https://github.com/Ritikpcl",
+      },
+    ]
+  }
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
